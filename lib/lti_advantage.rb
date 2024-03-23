aa8e74a52c89f2b7eb1d5e9ce70d4a66532ff4b9
Aaron F Stanton <afstanton@gmail.com>
2024-03-23 10:21:47 -0400

    Initial commit

---
+++ lib/lti_advantage.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lti_advantage/version"
+
+module LtiAdvantage
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/lti_advantage_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LtiAdvantage do
+  it "has a version number" do
+    expect(LtiAdvantage::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
